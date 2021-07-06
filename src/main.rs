90da746192e66e63ade6ba6d4a867cc0895a8394
Alden Garcia <76920905+rampus-bit@users.noreply.github.com>
2021-07-06 15:26:56 -0400

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,19 @@
-extern crate termion;
+use youchoose;
 
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
-use std::io::{Write, stdout, stdin};
+fn main(){
+    let mut menu = youchoose::Menu::new(0..100)
+    .preview(multiples);
+    let choice = menu.show();
+    println!("Chose {:?}", choice);
 
-fn main() {
-    // Get the standard input stream.
-    let stdin = stdin();
-    // Get the standard output stream and go to raw mode.
-    let mut stdout = stdout().into_raw_mode().unwrap();
+}
 
-    write!(stdout, "{}{}q to exit. Type stuff, use alt, and so on.{}",
-           // Clear the screen.
-           termion::clear::All,
-           // Goto (1,1).
-           termion::cursor::Goto(1, 1),
-           // Hide the cursor.
-           termion::cursor::Hide).unwrap();
-    // Flush stdout (i.e. make the output appear).
-    stdout.flush().unwrap();
-
-        for c in stdin.keys() {
-        // Clear the current line.
-        write!(stdout, "{}{}", termion::cursor::Goto(1, 1), termion::clear::CurrentLine).unwrap();
-
-        // Print the key we type...
-        match c.unwrap() {
-            // Exit.
-            Key::Char('q') => break,
-            Key::Left      => println!("<left>"),
-            Key::Right     => println!("<right>"),
-            Key::Up        => println!("<up>"),
-            Key::Down      => println!("<down>"),
-            _              => println!("Other"),
-        }
-
-        // Flush again.
-        stdout.flush().unwrap();
+fn multiples(num: i32) -> String {
+    let mut buffer = String::new();
+    for i in 0..20 {
+        buffer.push_str(
+            &format!("{} times {} is equal to {}!\n", num, i, num * i)
+        );
     }
-
-    // Show the cursor again before we exit.
-    write!(stdout, "{}", termion::cursor::Show).unwrap();
+    buffer
 }
