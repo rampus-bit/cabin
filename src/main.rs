e5b7a33787193923765f47de7895cf1a86e99294
Alden Garcia <76920905+rampus-bit@users.noreply.github.com>
2021-07-14 16:01:41 -0400

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,12 @@ mod ui;
 
 use std::io;
 use termion::raw::IntoRawMode;
-use termion::{event::Key};
 use termion::screen::AlternateScreen;
-use tui::widgets::{Widget, Block, Borders};
+use tui::widgets::{Block, Borders, Paragraph};
 use tui::{backend::TermionBackend, Terminal};
-use tui::layout::{Layout, Constraint, Direction};
+use tui::layout::{Alignment, Layout, Constraint, Direction};
+use tui::style::{Color, Modifier, Style};
+use tui::text::{Span, Spans};
 
 fn main() -> Result<(), io::Error> {
     let stdout = io::stdout().into_raw_mode()?;
@@ -26,13 +27,37 @@ fn main() -> Result<(), io::Error> {
                         Constraint::Percentage(10),
                         Constraint::Percentage(80),
                         Constraint::Percentage(10)
-                    ].as_ref()
+                    ]
+                    .as_ref()
                 )
                 .split(f.size());
-            let block = Block::default()
-                .title("Block")
-                .borders(Borders::ALL);
-            f.render_widget(block, chunks[0]);
+
+            let text = vec![
+                Spans::from("This is a line "),
+                Spans::from(Span::styled("This is a line   ", Style::default().fg(Color::Red))),
+                Spans::from(Span::styled("This is a line", Style::default().bg(Color::Blue))),
+                Spans::from(Span::styled(
+                    "This is a longer line",
+                    Style::default().add_modifier(Modifier::CROSSED_OUT),
+                )),
+                Spans::from(Span::styled(
+                    "This is a line",
+                    Style::default().fg(Color::Green).add_modifier(Modifier::ITALIC),
+                )),
+            ];
+
+            let create_block = |title| {
+                Block::default()
+                    .borders(Borders::ALL)
+                    .style(Style::default().bg(Color::White).fg(Color::Black))
+                    .title(Span::styled(title, Style::default().add_modifier(Modifier::BOLD)))
+            };
+
+            let paragraph = Paragraph::new(text.clone())
+                .style(Style::default().bg(Color::White).fg(Color::Black))
+                .block(create_block("Left, no wrap"))
+                .alignment(Alignment::Left);
+            f.render_widget(paragraph, chunks[0]);
 
             let block = Block::default()
                 .title("Block 2")
@@ -40,5 +65,4 @@ fn main() -> Result<(), io::Error> {
             f.render_widget(block, chunks[1]);
         })?;
     }
-    Ok(())
 }
